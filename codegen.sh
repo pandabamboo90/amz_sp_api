26df491cee325bad77029870b4473492a174b659
Dang Nguyen <dang.nguyen@comestri.com>
2022-02-22 15:13:30 +0700

    update codegen script to generate all the api modules

--- codegen.sh
+++ codegen.sh
@@ -3,13 +3,22 @@
 # exit on error
 set -e
 
-for FILE in `find ../selling-partner-api-models/models -name "*.json"`; do 
-	API_NAME=`echo $FILE | awk -F/ '{print $4}'`
+for FILE in `find ../selling-partner-api-models/models -name "*.json"`; do
+  # Find all json file and replace .json with empty string
+	API_NAME=`echo $FILE | awk -F/ '{print $5}' | sed -e "s/.json//g"`
+
+	# Set module name
 	MODULE_NAME=`echo $API_NAME | perl -pe 's/(^|-)./uc($&)/ge;s/-//g'`
 
+  # Convert camelCase to dash for creating dir
+	API_NAME=$(echo $API_NAME        \
+       | sed 's/\(.\)\([A-Z]\)/\1-\2/g' \
+       | tr '[:upper:]' '[:lower:]')
+
 	rm -rf lib/${API_NAME} || true
-	mkdir lib/$API_NAME
-	cp config.json lib/$API_NAME
+	mkdir -p lib/${API_NAME}
+	cp config.json lib/${API_NAME}
+
 	sed -i '' "s/GEMNAME/${API_NAME}/g" lib/${API_NAME}/config.json
 	sed -i '' "s/MODULENAME/${MODULE_NAME}/g" lib/${API_NAME}/config.json
 
